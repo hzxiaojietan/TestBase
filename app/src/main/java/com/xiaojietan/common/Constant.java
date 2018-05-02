094adb04954db7afe8559e3a9bc37db76bec9388
xiaojietan&jake <jake618@126.com>
2018-05-02 16:41:09 +0800

    删除无用代码

--- app/src/main/java/com/xiaojietan/common/Constant.java
+++ app/src/main/java/com/xiaojietan/common/Constant.java
@@ -17,21 +17,4 @@ public class Constant {
             + "/video/";
 
     public static final String SERVER_BASE_URL = BuildConfig.HTTP_SERVER_URL;;
-    public static final String FILE_BASE_URL = SERVER_BASE_URL + "p/download/";
-
-    //性别
-    public static final String GENDER_MALE = "male";
-    public static final String GENDER_FEMALE = "female";
-
-    public static final int TIME_MINUTE = 60000;
-    public static final int ONE_MINUTE = 1000;
-
-    //语音设置常量
-    public static final String AUDIO_SETTING_PREFERENCE_KEY = "voice_item";
-    public static final String AUDIO_SETTING_ALLOW_ALL = "2";
-    public static final String AUDIO_SETTING_ONLY_SYSTEM_MESSAGE = "1";
-    public static final String AUDIO_SETTING_CLOSE_ALL = "0";
-
-    // 标识服务Fragment是否需要重新刷新获取数据
-    public static boolean SERVICE_FRAGMENT_IS_REFRESH = false;
 }
--- app/src/main/java/com/xiaojietan/common/TestBaseApplication.java
+++ app/src/main/java/com/xiaojietan/common/TestBaseApplication.java
@@ -2,7 +2,6 @@ package com.xiaojietan.common;
 
 import android.content.Context;
 import android.os.Build;
-import android.os.Handler;
 
 import com.xiaojietan.testbase.BuildConfig;
 
@@ -16,15 +15,10 @@ import cn.hzxiaojietan.base.common.utils.AppLog;
  * Created by xiaojie.tan on 2017/10/27
  */
 public class TestBaseApplication extends LitePalApplication {
-
     private static final String TAG = "TestBaseApplication";
-    public static final String SAVED_CITY = "_saved_city";
-    private static final long DELAY_TIME = 1000*60*20;// 20分钟
     private static Context context;
     public static String MOBILE_TYPE;
     public static String OS_VERSION;
-    public static long downloadId; // 更新下载APP，下载id(自动安装需要)
-    private Handler mHandler;
 
     public void onCreate() {
         super.onCreate();
@@ -39,7 +33,6 @@ public class TestBaseApplication extends LitePalApplication {
 
         AppLog.init(BuildConfig.DEBUG);
     }
-
 
     public static Context getAppContext() {
         return TestBaseApplication.context;
